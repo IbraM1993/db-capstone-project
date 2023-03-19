cc3d922a27f61f919b3eb19e8683b94d0dc29e7c
BiboSS4 <ibrahim.maassarani@gmail.com>
2023-03-19 20:21:23 +0200

    Adding ex 1 scripts with screenshots and taking a break!

---
+++ week2/table_booking/ex1/task1.sql
@@ -0,0 +1,7 @@
+INSERT INTO bookings (date, table_number, customer_id)
+VALUES 	("2022-3-10", 5, 1),
+		("2022-3-11", 3, 3),
+		("2022-3-12", 2, 2),
+		("2022-3-13", 2, 1);
+        
+SELECT * FROM bookings;
